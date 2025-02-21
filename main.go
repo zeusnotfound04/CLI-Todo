fe153ea3c0ae497f48b1817f1ac9f81a63261d76
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-02-22 03:17:55 +0530

    added commands in the cli

--- main.go
+++ main.go
@@ -7,9 +7,7 @@ func main() {
 	todos := Todos{}
 	storage := NewStorage[Todos]("todos.json")
 	storage.Load(&todos)
-	todos.add("Buy Pussy")
-	todos.add("Buy Licker")
-	todos.toggle(0)
-	todos.print()
+	cmdFlags := NewCmdFlags()
+	cmdFlags.Execute(&todos)
 	storage.Save(todos)
 }
