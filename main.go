584f5cfee12dc5f2fdae6d455c22bb6f54d89f69
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-02-22 02:42:55 +0530

    added storage

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ package main
 
 func main() {
 	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
 	todos.add("Buy Pussy")
 	todos.add("Buy Licker")
 	todos.toggle(0)
 	todos.print()
-
+	storage.Save(todos)
 }
